7b43c7f35952a6098435e80407ab460d5f739a63
lukitsha <lucagaido_@hotmail.com>
2024-02-29 22:16:46 -0300

    final

--- js/main.js
+++ js/main.js
@@ -1,146 +1,209 @@
-// Ignorar ()[]{} \n
 
-/*
-function solicitarPrecio () {
-    let precioProducto = Number(prompt('Ingrese el Precio'))
-    alert('El precio ingresado es $' + precioProducto)
+// Cambio de Modo de Color
+
+let darkMode = localStorage.getItem("dark-mode")
+
+const body = document.querySelector("body")
+const div = document.querySelector("div")
+const calculadoraIntereses = document.querySelector("#calculadoraIntereses")
+const darkModeButton = document.querySelector("#color-dark")
+const lightModeButton = document.querySelector("#color-light")
+const changeModeButton = document.querySelector("#color-change")
+const recuadrosConversor1 = document.querySelector("#cambioColor1")
+const recuadrosConversor2 = document.querySelector("#cambioColor2")
+const recuadrosConversor3 = document.querySelector("#cambioColor3")
+const recuadrosConversor4 = document.querySelector("#cambioColor4")
+const recuadrosConversor5 = document.querySelector("#cambioColor5")
+
+
+darkModeButton.addEventListener("click", cambiarModoOscuro);
+darkModeButton.addEventListener("click", () => {
+    Toastify({
+        text: "Modo Oscuro Seleccionado!",
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", 
+        position: "right", 
+        stopOnFocus: true,
+        style: {
+          background: "linear-gradient(to right, #5a0170, #9a04bf)",
+        },
+        onClick: function(){}
+      }).showToast();
+       
+}) 
+
+function cambiarModoOscuro() {
+    body.classList.add("dark-mode")
+    calculadoraIntereses.classList.add("addDarkMode")
+    recuadrosConversor1.classList.add("divisaDark")
+    recuadrosConversor2.classList.add("divisaDark")
+    recuadrosConversor3.classList.add("divisaDark")
+    recuadrosConversor4.classList.add("divisaDark")
+    recuadrosConversor5.classList.add("divisaDark")
+ 
+    localStorage.setItem("dark-mode", "activado")
 }
-solicitarPrecio()
-function solicitarIva () {
-    let iva = Number(prompt('Ingrese el Monto de IVA'))
-    alert('El IVA ingresado es %' + iva)
+
+lightModeButton.addEventListener("click", cambiarModoClaro);
+lightModeButton.addEventListener("click", () => {
+    Toastify({
+        text: "Modo Claro Seleccionado!",
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", 
+        position: "right", 
+        stopOnFocus: true, 
+        style: {
+          background: "linear-gradient(to right, #017c82, #02b2ba)",
+        },
+        onClick: function(){}
+      }).showToast();
+       
+}) 
+
+function cambiarModoClaro() {
+    body.classList.remove("dark-mode")
+    calculadoraIntereses.classList.remove("addDarkMode")
+    recuadrosConversor1.classList.remove("divisaDark")
+    recuadrosConversor2.classList.remove("divisaDark")
+    recuadrosConversor3.classList.remove("divisaDark")
+    recuadrosConversor4.classList.remove("divisaDark")
+    recuadrosConversor5.classList.remove("divisaDark")
+    localStorage.setItem("dark-mode", "desactivado")
 }
-solicitarIva()
-function sumar () {
-    let precioFinal = precioProducto + iva
-    alert('El precio final es de $' + precioFinal)
+
+if (darkMode === "activado") {
+    cambiarModoOscuro();
+} else {
+    cambiarModoClaro();
 }
 
-sumar()
+changeModeButton.addEventListener("click", () =>{
+    darkMode = localStorage.getItem("dark-mode");
 
-*/
-
-//Funciones
-
-
-function saludar () {
-    let nombre = prompt("Porfavor, Ingrese su nombre:")
-    alert(
-        "Bienvenido/a " + nombre + " a nuestro almacén en línea"
-    )
-    alert(
-        "A continuación, seleccione los productos que desea agregar al carrito"
-        )
-}
-
-let productoCarrito
-function agregarProducto () {
-    productoCarrito = parseInt(prompt(
-        "Lista de Productos: \n 1: NVidia GForce GTX 1660 ti \n 2: NVidia GForce RTX 2080 ti \n 3: NVidia GForce RTX 3080 ti \n 4: NVidia GForce RTX 4090")
-        )
+    if (darkMode === "activado") {
+        cambiarModoClaro();
+        Toastify({
+            text: "Modo Claro Seleccionado!",
+            duration: 3000,
+            destination: "https://github.com/apvarun/toastify-js",
+            newWindow: true,
+            close: true,
+            gravity: "top", 
+            position: "right", 
+            stopOnFocus: true, 
+            style: {
+              background: "linear-gradient(to right, #017c82, #02b2ba)",
+            },
+            onClick: function(){}
+          }).showToast();
+    } else {
+        cambiarModoOscuro();
+        Toastify({
+            text: "Modo Oscuro Seleccionado!",
+            duration: 3000,
+            destination: "https://github.com/apvarun/toastify-js",
+            newWindow: true,
+            close: true,
+            gravity: "top", 
+            position: "right", 
+            stopOnFocus: true, 
+            style: {
+              background: "linear-gradient(to right, #5a0170, #9a04bf)",
+            },
+            onClick: function(){} 
+          }).showToast();
+    }
+})
     
-    if (productoCarrito === 1) {
-        alert("Seleccionasate NVidia GForce GTX 1660 ti")
-    } else if (productoCarrito === 2) {
-        alert("Seleccionasate NVidia GForce RTX 2080 ti")
-    } else if (productoCarrito === 3) {
-        alert("Seleccionasate NVidia GForce RTX 3080")
-    } else if (productoCarrito === 4) {
-        alert("Seleccionasate NVidia GForce RTX 4090")
-    } else alert("La opcion seleccionada no es valida")
-}
-
-function finalizarCompra () {
-    if (productoCarrito == 1) {
-        alert("El precio final de tu NVidia GForce GTX 1660 ti es " + gtx1660 * 1.21)
-    } else if (productoCarrito == 2) {
-        alert("El precio final de tu NVidia GForce RTX 2080 ti es " + rtx2080ti * 1.21)
-    } else if (productoCarrito == 3) {
-        alert("El precio final de tu NVidia GForce RTX 3080 es " + rtx3080 * 1.21)
-    } else if (productoCarrito == 4) {
-        alert("El precio final de tu NVidia GForce RTX 4090 es " + rtx4090 * 1.21)
-    } else alert("La opcion seleccionada no es valida")    
+function cambiarModoActual() {
+    body.classList.toggle("dark-mode")
+    calculadoraIntereses.classList.toggle("addDarkMode")
 }
 
 
+//Calculadora de intereses
 
-//Main 
+function computeLoan(){
+     // Calculr tasa de Interes
+    const amount = document.querySelector('#amount').value;
+    const interest_rate = document.querySelector('#interest_rate').value;
+    const months = document.querySelector('#months').value
+    // Calcular Interes por mes
+    const interest = (amount * (interest_rate * 0.01)) / months;
+    // Dividir el interes total entre la cantidd de meses, y gregarlo al costo mensual
+    let payment = ((amount / months) + interest).toFixed(2);
 
-saludar()
-agregarProducto()
+    // Aregar una coma(,) cada tres numeros
+    payment = payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-
-let confirmarContinuacion = parseInt(prompt(
-    "Que desea realizar a continuacion? \n 1: Finalizar Compra \n 2: Continuar Comprando \n 3: Cancelar Compra")
-)
-
-let gtx1660 = 70000
-let rtx2080ti = 145000
-let rtx3080 = 190000
-let rtx4090 = 340000
-
-
-while (confirmarContinuacion !== 3) {
-    if (confirmarContinuacion === 1) {
-        finalizarCompra()
-        alert("A continuacion veras tu carrito para finalizar tu compra")
-        alert("*SE MOSTRARIA EL CARRITO*")
-        break
-    } else if (confirmarContinuacion === 2) {
-        agregarProducto()
-        
-    } 
-    confirmarContinuacion = parseInt(prompt(
-        "Que desea realizar a continuacion? \n 1: Finalizar Compra \n 2: Continuar Comprando \n 3: Cancelar Compra")
-    )
+    // Usamos DOM para mandar el valor total correspondiente al Pago Mensual
+    document.querySelector('#payment').innerHTML = `Pago Mensual = ${payment}`
 }
-alert("Gracias por utilizar nuestro servicio! Esperamos verlo nuevamente por aqui muy pronto!")
 
 
-// Objetos 
-// let gtx1660 = 70000
-// let rtx2080ti = 145000
-// let rtx3080 = 190000
-// let rtx4090 = 340000
 
-class productosCarrito {
-    constructor(nombre,categoria,precio){
-    this.nombre = nombre
-    this.categoria = categoria
-    this.precio = precio
-    }
+// CONVERSOR DIVISAS LOCAL
 
-    aplicarImpuesto () {
-        console.log('el precio con Impuestos es de ' + this.precio * 1.21)
-    }
-    aplicarDescuento () {
-        console.log('el precio con Descuento es de ' + this.precio * 0.90)
-    }
-        
+let inputs = document.querySelectorAll(".valor");
+inputs.forEach((input) => {
+  input.value = input.dataset.cambio;  
+});
+
+function valorModificado(input) {
+    let factor = input.value / input.dataset.cambio;
+    inputs.forEach(campo => {
+      campo.value = (campo.dataset.cambio * factor).toFixed(2);
+    })
+}
+
+
+ 
+// CONVERSOR DIVISAS API
+
+const monedaEl_one = document.getElementById('moneda-uno');
+const monedaEl_two = document.getElementById('moneda-dos');
+const cantidadEl_one = document.getElementById('cantidad-uno');
+const cantidadEl_two = document.getElementById('cantidad-dos');
+const cambioEl = document.getElementById('cambio');
+const tazaEl = document.getElementById('taza');
+
+
+
+function calculate(){
+    const moneda_one = monedaEl_one.value;
+    const moneda_two = monedaEl_two.value;
+
+   fetch(`https://api.exchangerate-api.com/v4/latest/${moneda_one}`)
+   .then(res => res.json() )
+   .then(data => {
+       const taza = data.rates[moneda_two];
+       
+       cambioEl.innerText = `1 ${moneda_one} = ${taza} ${moneda_two}`;
+
+       cantidadEl_two.value = (cantidadEl_one.value * taza).toFixed(2);
+
+    } );
     
 }
 
-const producto1 = new productosCarrito ('gtx1660','placas de video',70000)
-const producto2 = new productosCarrito ('rtx2080ti','placas de video',145000)
-const producto3 = new productosCarrito ('rtx3080','placas de video',190000)
-const producto4 = new productosCarrito ('rtx4090','placas de video',340000)
+monedaEl_one.addEventListener('change', calculate);
+cantidadEl_one.addEventListener('input', calculate);
+monedaEl_two.addEventListener('change', calculate);
+cantidadEl_two.addEventListener('input', calculate);
 
-console.log(producto1)
-console.log(producto2)
-console.log(producto3)
-console.log(producto4)
-
-producto1.aplicarImpuesto()
-producto2.aplicarImpuesto()
-producto3.aplicarImpuesto()
-
-console.log(producto1)
-console.log(producto2)
-console.log(producto3)
-console.log(producto4)
-
-producto4.aplicarDescuento()
+taza.addEventListener('click', () =>{
+    const temp = monedaEl_one.value;
+    monedaEl_one.value = monedaEl_two.value;
+    monedaEl_two.value = temp;
+    calculate();
+} );
 
 
-console.log(producto4)
-
+calculate();
+  
